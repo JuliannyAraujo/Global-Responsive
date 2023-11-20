ec3c72b3e530cb89d4af645f2b2c4cba342f75b3
JuliannyAraujo <rm99554@fiap.com.br>
2023-11-20 11:58:36 -0300

    alterando pagina login

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-function loadPage(page) {
-    const contentSection = document.getElementById('content');
+document.addEventListener('DOMContentLoaded', function () {
+    // Adiciona eventos de clique para os links de navegação
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault(); // Evita que o link recarregue a página
+            loadPage(this.getAttribute('data-page')); // Carrega a nova página
+        });
+    });
+});
 
-    fetch(`${page}.html`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Erro ao carregar ${page}.html`);
-            }
-            return response.text();
-        })
-        .then(data => {
-            contentSection.innerHTML = data;
-            // Adiciona um evento de clique para os links na nova página carregada
-            document.querySelectorAll('nav a').forEach(link => {
-                link.addEventListener('click', function(e) {
-                    e.preventDefault(); // Evita que o link recarregue a página
-                    loadPage(this.getAttribute('data-page')); // Carrega a nova página
-                });
-            });
-        })
-        .catch(error => console.error(error));
+function loadPage(page) {
+    // Redireciona para a página especificada
+    window.location.href = `${page}.html`;
 }
 
 function criarConta() {
